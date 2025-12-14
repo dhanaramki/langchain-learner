6b4569d324dc2d1d9ea19254f1c4019b01edf756
dhanaramki <dhanasekaran.ramakrishnan@gmail.com>
2025-12-14 12:14:19 +0530

    Langchain-React

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="openai/gpt-4o", api_key=os.getenv("OPENROUTER_API_KEY"), base_url="https://openrouter.ai/api/v1")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
+def main():
+
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
